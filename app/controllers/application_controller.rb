1d4ed657f94ac614ea005511228a3ef4519f374b
Himanshu Gupta <cephalus.mail@gmail.com>
2023-12-03 20:46:21 +0530

    fix: clean up views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+  rescue_from ActiveRecord::RecordNotFound, with: :error_not_found
+
+  def error_not_found
+    render file: "#{Rails.root}/public/404.html", status: 404
+  end
 end
--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -6,23 +6,14 @@ class CitiesController < ApplicationController
     @cities = City.all
   end
 
-  #Todo - have a 404 here
-  def show
-    @city = City.find_by(id: params[:id])
-  end
-
   def average_monthly_aqi
     @average_aqi_hash = @city.average_monthly_aqi
   end
 
-
   private
 
   def find_city
-    @city = City.find_by(id: params[:city_id])
+    @city = City.find_by(id: params[:city_id] || params[:id])
   end
 
-  def permitted_resource_params
-    params.require(:city).permit(:id)
-  end
 end
